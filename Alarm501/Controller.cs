67fa4584e4fb0c3139f4f9924016a80292cb835f
Yuto Wada <yuto808263@vet.k-state.edu>
2022-03-01 14:54:04 -0600

    Implementing MVC architecture

---
+++ Alarm501/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm501
+{
+    class Controller
+    {
+    }
+}
