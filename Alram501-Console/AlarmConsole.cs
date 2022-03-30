5eb68c6e10100150acb78a6d24dd220827852497
Yuto Wada <yuto808263@vet.k-state.edu>
2022-03-30 16:53:15 -0500

    Started logic in colsole

--- Alram501-Console/AlarmConsole.cs
+++ Alram501-Console/AlarmConsole.cs
@@ -8,7 +8,37 @@ namespace Alram501_Console
     {
         static void Main(string[] args)
         {
-            
+            string option;
+            bool setting = true;
+            while (setting)
+            {
+                Console.WriteLine("Add (a), Edit(e), Stop(s), Snooze(s), Quit(q)");
+                option = Console.ReadLine().ToLower();
+                //List out all the alarm in the text file.
+                //Ask user what they want to do with the alarm.
+                //Options are Add, Edit, Stop, and Snooze.
+                //If (add)
+                //If (edit)
+                if(option == "a")
+                {
+                    Console.WriteLine("Add");
+                }
+                if(option == "e")
+                {
+                    Console.WriteLine("Edit");
+                }
+                if (option == "s")
+                {
+                    Console.WriteLine("Snoose");
+                }
+                if(option == "q")
+                {
+                    Console.WriteLine("Quit");
+                    setting = false;
+                }
+
+            }
+
             
         }
     }
